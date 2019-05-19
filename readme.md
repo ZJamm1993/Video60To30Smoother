0a46c4e684f9eac6f87249b2475a5db93416a5b8
zhangjingjian <zhangjingjian@dabby.cn>
2019-05-19 18:53:18 +0800

    add demo video

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-make 60fps to 30fps video smoother !
+make 60fps to 30fps video smoother ?
+how to fix serial frames to make it look smoother?
--- video60to30smoother.py
+++ video60to30smoother.py
@@ -15,38 +15,49 @@ cc = cv2.VideoWriter_fourcc(*'X264')
 savefilename = 'out30_' + time.strftime('%Y%m%d_%H%M%S') + '.mp4'
 out = cv2.VideoWriter(savefilename,cc,30,(int(width),int(height)))
 
+using3frame = '3'
+using2frame = '2'
+option = using2frame
+
 frameHead = None
 frameMidd = None
 frameTail = None
-tag = 0
 currentframeindex = 0
 while cap.isOpened():
     ret, frame = cap.read()
     if ret == False:
         break
-    frameHead = frameMidd
-    frameMidd = frameTail
-    frameTail = frame
-    if currentframeindex % 2 == 0 and currentframeindex > 0:
-        firstAlpha = 0.5
-        newFrame = None
-        if frameHead.any() :
-            newFrame = cv2.addWeighted(frameHead, firstAlpha, frameTail,1-firstAlpha,0)
+    
+    if option == using3frame:
+        frameHead = frameMidd
+        frameMidd = frameTail
+        frameTail = frame
+        if currentframeindex % 2 == 0 and currentframeindex > 0:
+            firstAlpha = 0.8
+            newFrame = None
+            if frameHead.any() :
+                newFrame = cv2.addWeighted(frameHead, firstAlpha, frameTail,1-firstAlpha,0)
+            else :
+                newFrame = frameTail
+            firstAlpha = 0.2
+            newFrame = cv2.addWeighted(frameMidd, firstAlpha,newFrame,1-firstAlpha,0)
+            # create a new frame with 3 old frames
+            # how to make it smoother??
+            out.write(newFrame)
+
+    elif option == using2frame:
+        if currentframeindex % 2 == 0:
+            frameMain = frame
         else :
-            newFrame = frameTail
-        firstAlpha = 0.7
-        newFrame = cv2.addWeighted(frameMidd,firstAlpha,newFrame,1-firstAlpha,0)
-        # create a new frame with 3 old frames
+            firstAlpha = 0.7
+            frame = cv2.addWeighted(frameMain,firstAlpha,frame,1-firstAlpha,0)
+            # create a new frame with 2 old frams
+            out.write(frame)
 
-        # how to make it smoother??
-
-        # cv2.imshow('frame',frame)
-        out.write(newFrame)
-        if currentframeindex % 10 == 0:
-            print 'progress:'+str(currentframeindex)+'/'+str(framecount)
+    # print and loop
+    print 'progress:'+str(currentframeindex)+'/'+str(framecount)
     currentframeindex += 1
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    
 cap.release()
 out.release()
 print 'finish'
