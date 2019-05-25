2aac895a7852bf22d15e101c1ca72db1bffee6b7
zhangjingjian <zhangjingjian@dabby.cn>
2019-05-25 23:36:35 +0800

    改成“隔行扫描”的效果

--- video60to30smoother.py
+++ video60to30smoother.py
@@ -1,13 +1,14 @@
 print 'hello'
 
 from cv2 import cv2
+import numpy as np
 import time
 
 cap = cv2.VideoCapture('test60.mp4')
 
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-framecount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+framecount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 print 'total frame: ' + str(framecount)
 
@@ -50,9 +51,18 @@ while cap.isOpened():
             frameMain = frame
         else :
             firstAlpha = 0.7
-            frame = cv2.addWeighted(frameMain,firstAlpha,frame,1-firstAlpha,0)
+            # frame = cv2.addWeighted(frameMain,firstAlpha,frame,1-firstAlpha,0)
+            newFrame = np.zeros((height, width,3),np.uint8)
+            for i in range(height):
+                if i % 2 == 0:
+                    newFrame[i] = frameMain[i]
+                else :
+                    newFrame[i] = frame[i]
+                    
             # create a new frame with 2 old frams
-            out.write(frame)
+            # cv2.imshow('1', newFrame)
+            # cv2.waitKey(0)
+            out.write(newFrame)
 
     # print and loop
     print 'progress:'+str(currentframeindex)+'/'+str(framecount)
